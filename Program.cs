5ca87a9f5ed5550933141f907b0f4cef7c157318
Jaqueline <66227952+jaquelinesilfe@users.noreply.github.com>
2021-10-19 15:56:36 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Enum.Entities.Enums;
+using Enum.Entities;
+
+
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+
+            };
+            Console.WriteLine(order);
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            //OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
+        }
+    }
+}
